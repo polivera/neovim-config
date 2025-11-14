8ba566999d09e1b28c4969567eb96e4715a65b6b
polivera <punk.moon2229@fastmail.com>
2025-11-14 17:34:44 +0100

    Change color theme to gruvbox

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,24 @@
 -- Tokyo night color scheme
 -- @see https://github.com/folke/tokyonight.nvim
 
+-- Tokyo night
+-- return {
+--     "folke/tokyonight.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         vim.cmd([[colorscheme tokyonight]])
+--     end,
+-- }
+
+-- Gruvbox
 return {
-    "folke/tokyonight.nvim",
-    lazy = false,
+    "ellisonleao/gruvbox.nvim",
     priority = 1000,
     config = function()
-        vim.cmd([[colorscheme tokyonight]])
+        require("gruvbox").setup({
+            contrast = "hard",
+        })
+        vim.cmd("colorscheme gruvbox")
     end,
 }
