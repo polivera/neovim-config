9fe189c26f2f1e305f0b9cb83b9b5cb5748a2d4d
polivera <punk.moon2229@fastmail.com>
2025-11-26 22:27:14 +0100

    Change color theme to tokyonight

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,23 +2,36 @@
 -- @see https://github.com/folke/tokyonight.nvim
 
 -- Tokyo night
+return {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.cmd([[colorscheme tokyonight-night]])
+    end,
+}
+
+-- Gruvbox
 -- return {
---     "folke/tokyonight.nvim",
---     lazy = false,
+--     "ellisonleao/gruvbox.nvim",
 --     priority = 1000,
 --     config = function()
---         vim.cmd([[colorscheme tokyonight]])
+--         require("gruvbox").setup({
+--             contrast = "hard",
+--         })
+--         vim.cmd("colorscheme gruvbox")
 --     end,
 -- }
 
--- Gruvbox
-return {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-        require("gruvbox").setup({
-            contrast = "hard",
-        })
-        vim.cmd("colorscheme gruvbox")
-    end,
-}
+-- Sonokai
+-- return {
+--     "sainnhe/sonokai",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         -- Optionally configure and load the colorscheme
+--         -- directly inside the plugin declaration.
+--         vim.g.sonokai_enable_italic = true
+--         vim.cmd.colorscheme("sonokai")
+--     end,
+-- }
--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -6,6 +6,7 @@ return {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope.nvim",
     },
+    lazy = false,
     opts = {},
     config = function()
         require("todo-comments").setup()
